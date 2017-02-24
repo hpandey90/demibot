d06c1c275ee0a25b90543ca2a118ff0dd55a361d
BrajGopalMaity <brajgopal90@gmail.com>
2017-02-24 00:52:47 -0500

    changes made

--- data.py
+++ data.py
@@ -11,11 +11,12 @@ import numpy as np
     2. Create a dictionary with ( key = line_id, value = text )
 '''
 def get_id2line():
-    lines = open('./data/raw_data/movie_lines.txt', encoding='utf-8', errors ='ignore').read().split('/n')
+    lines = open('./data/raw_data/movie_lines.txt', encoding='utf-8', errors ='ignore').read().split('\n')
     id2line = {}
     for line in lines:
         _line = line.split("+++$+++")
-        id2line[_line[0]] = _line[4]
+        if len(_line) == 5:
+            id2line[_line[0]] = _line[4]
     return id2line
 
 
@@ -24,7 +25,7 @@ def get_id2line():
     2. Create a list of [list of line_id's]
 '''
 def get_conversations():
-    conv = open('./data/raw_data/movie_conversations.txt', encoding='utf-8', errors ='ignore').read().split('/n')
+    conv = open('./data/raw_data/movie_conversations.txt', encoding='utf-8', errors ='ignore').read().split('\n')
     convs = []
     for line in conv[:-1]:
         _line = line.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
