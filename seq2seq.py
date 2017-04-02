cbf604dff93648ac6cfa2d42144975ae3e437016
ashish4feb <katiyar.ashish1989@gmail.com>
2017-04-02 00:34:20 -0400

    seq2seq file added

---
+++ seq2seq.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy as np
+
+
+
+class Seq2Seq(object):
+
+    def __init__(self, xseq_len, yseq_len,
+            xvocab_size, yvocab_size,
+            emb_dim, num_layers, ckpt_path,
+            lr=0.0001,
+            epochs=100, model_name='seq2seq_model'):
+
+        # attach these arguments to self
+        self.xseq_len = xseq_len
+        self.yseq_len = yseq_len
+        self.ckpt_path = ckpt_path
+        self.epochs = epochs
+        self.model_name = model_name
