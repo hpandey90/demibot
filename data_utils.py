8a0618b7fdefada3842592367a18aed98c756f96
ashish4feb <katiyar.ashish1989@gmail.com>
2017-04-08 03:41:26 -0400

    random batch generator

--- data_utils.py
+++ data_utils.py
@@ -1,2 +1,13 @@
 import numpy as np
 from random import sample
+
+
+'''
+ generate batches, by random sampling a bunch of items
+    yield (x_gen, y_gen)
+
+'''
+def rand_batch_gen(x, y, batch_size):
+    while True:
+        sample_idx = sample(list(np.arange(len(x))), batch_size)
+        yield x[sample_idx].T, y[sample_idx].T
