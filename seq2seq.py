4691f14185c479bfe2e0064a109ba329e8f86b9d
BrajGopalMaity <brajgopal90@gmail.com>
2017-04-08 02:39:32 -0400

    seq2seq

--- seq2seq.py
+++ seq2seq.py
@@ -1,19 +1,33 @@
-import tensorflow as tf
-import numpy as np
+    def get_feed(self, X, Y, keep_prob):
+        feed_dict = {self.enc_ip[t]: X[t] for t in range(self.xseq_len)}
+        feed_dict.update({self.labels[t]: Y[t] for t in range(self.yseq_len)})
+        feed_dict[self.keep_prob] = keep_prob # dropout prob
+        return feed_dict
 
+    # run one batch for training
+    def train_batch(self, sess, train_batch_gen):
+        # get batches
+        batchX, batchY = train_batch_gen.__next__()
+        # build feed
+        feed_dict = self.get_feed(batchX, batchY, keep_prob=0.5)
+        _, loss_v = sess.run([self.train_op, self.loss], feed_dict)
+        return loss_v
 
+    def eval_step(self, sess, eval_batch_gen):
+        # get batches
+        batchX, batchY = eval_batch_gen.__next__()
+        # build feed
+        feed_dict = self.get_feed(batchX, batchY, keep_prob=1.)
+        loss_v, dec_op_v = sess.run([self.loss, self.decode_outputs_test], feed_dict)
+        # dec_op_v is a list; also need to transpose 0,1 indices
+        #  (interchange batch_size and timesteps dimensions
+        dec_op_v = np.array(dec_op_v).transpose([1,0,2])
+        return loss_v, dec_op_v, batchX, batchY
 
-class Seq2Seq(object):
-
-    def __init__(self, xseq_len, yseq_len,
-            xvocab_size, yvocab_size,
-            emb_dim, num_layers, ckpt_path,
-            lr=0.0001,
-            epochs=100, model_name='seq2seq_model'):
-
-        # attach these arguments to self
-        self.xseq_len = xseq_len
-        self.yseq_len = yseq_len
-        self.ckpt_path = ckpt_path
-        self.epochs = epochs
-        self.model_name = model_name
+    # evaluate 'num_batches' batches
+    def eval_batches(self, sess, eval_batch_gen, num_batches):
+        losses = []
+        for i in range(num_batches):
+            loss_v, dec_op_v, batchX, batchY = self.eval_step(sess, eval_batch_gen)
+            losses.append(loss_v)
+        return np.mean(losses)
