25dac628a6874ed8633e397c1dd56dca69fe5789
BrajGopalMaity <brajgopal90@gmail.com>
2017-04-08 03:18:41 -0400

    demibot

--- demibot.py
+++ demibot.py
@@ -9,3 +9,11 @@ import data_utils
 # load data from pickle and npy files
 metadata, idx_q, idx_a = data.load_data(PATH='')
 (trainX, trainY), (testX, testY), (validX, validY) = data_utils.split_dataset(idx_q, idx_a)
+
+# parameters
+xseq_len = trainX.shape[-1]
+yseq_len = trainY.shape[-1]
+batch_size = 10
+xvocab_size = len(metadata['idx2w'])
+yvocab_size = xvocab_size
+emb_dim = 1024
