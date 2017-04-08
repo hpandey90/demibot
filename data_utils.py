e1c84e5e689d47d5a39b013936fdc711ff86ae6f
himanshu pandey <hpandey90@gmail.com>
2017-04-08 03:44:26 -0400

    batch generation code added

--- data_utils.py
+++ data_utils.py
@@ -9,3 +9,10 @@ def split_dataset(x, y, ratio = [0.7, 0.15, 0.15] ):
     # number of examples
     data_len = len(x)
     lens = [ int(data_len*item) for item in ratio ]
+
+
+# generate sample batches from dataset 
+
+def batch_gen(x, y, batch_size):
+    while True:
+        for i in range(0, len(x), batch_size):
