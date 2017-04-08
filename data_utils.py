54a2b7702135a5275e78c9a2e6c04c18f4b711ee
ashish4feb <katiyar.ashish1989@gmail.com>
2017-04-08 03:47:44 -0400

    decode words and combine to form sentence

--- data_utils.py
+++ data_utils.py
@@ -24,3 +24,8 @@ def rand_batch_gen(x, y, batch_size):
     while True:
         sample_idx = sample(list(np.arange(len(x))), batch_size)
         yield x[sample_idx].T, y[sample_idx].T
+
+#Decode and combine the words
+def decode(sequence, lookup, separator=''): # 0 used for padding, is ignored
+    #print([ lookup[element] for element in sequence if element ])
+    return separator.join([ lookup[element] for element in sequence if element ])
