33f9ff03a18a63556baf99bd614a54046264f0f1
BrajGopalMaity <brajgopal90@gmail.com>
2017-04-08 03:17:08 -0400

    demibot

--- demibot.py
+++ demibot.py
@@ -5,3 +5,7 @@ import seq2seq_wrapper
 # preprocessed data
 import data
 import data_utils
+
+# load data from pickle and npy files
+metadata, idx_q, idx_a = data.load_data(PATH='')
+(trainX, trainY), (testX, testY), (validX, validY) = data_utils.split_dataset(idx_q, idx_a)
