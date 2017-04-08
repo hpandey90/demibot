a9ba0d38a7aa13c26de3c7c7d9a9bc6b94a55b55
BrajGopalMaity <brajgopal90@gmail.com>
2017-04-08 03:40:28 -0400

    data_utils

--- data_utils.py
+++ data_utils.py
@@ -1,2 +1,7 @@
 import numpy as np
 from random import sample
+
+'''
+ split data into train (75%), test (15%) and valid(10%)
+    return tuple( (trainX, trainY), (testX,testY), (validX,validY) )
+'''
