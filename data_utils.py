aa1babf4a73f161edd3ada386ec076b54b0fd90c
BrajGopalMaity <brajgopal90@gmail.com>
2017-04-08 03:42:46 -0400

    data_utils

--- data_utils.py
+++ data_utils.py
@@ -5,3 +5,7 @@ from random import sample
  split data into train (75%), test (15%) and valid(10%)
     return tuple( (trainX, trainY), (testX,testY), (validX,validY) )
 '''
+def split_dataset(x, y, ratio = [0.7, 0.15, 0.15] ):
+    # number of examples
+    data_len = len(x)
+    lens = [ int(data_len*item) for item in ratio ]
