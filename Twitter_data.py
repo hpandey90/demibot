74ba9995d4b0fe01a2f1ec4f35434374ce26381b
ashish4feb <katiyar.ashish1989@gmail.com>
2017-04-11 03:39:16 -0400

    Twitter data processing

---
+++ Twitter_data.py
@@ -0,0 +1,12 @@
+UNK = 'unk'
+VOCAB_SIZE = 8000
+limit = {
+        'maxq' : 25,
+        'minq' : 2,
+        'maxa' : 25,
+        'mina' : 2
+        }
+
+EN_WHITELIST = '0123456789abcdefghijklmnopqrstuvwxyz ' # space included
+
+FILENAME = 'data/raw_data/twitter/chat.txt'
