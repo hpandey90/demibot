46bbe67e3e1a37395bd69d7822533d1021e4c08e
ashish4feb <katiyar.ashish1989@gmail.com>
2017-04-11 03:40:35 -0400

    Read Lines

--- Twitter_data.py
+++ Twitter_data.py
@@ -10,3 +10,20 @@ limit = {
 EN_WHITELIST = '0123456789abcdefghijklmnopqrstuvwxyz ' # space included
 
 FILENAME = 'data/raw_data/twitter/chat.txt'
+
+
+import random
+import sys
+
+import nltk
+import itertools
+from collections import defaultdict
+
+import numpy as np
+
+import pickle
+
+
+# read lines from file returns [list of lines]
+def read_lines(filename):
+    return open(filename,encoding='utf-8').read().split('\n')[:-1]
