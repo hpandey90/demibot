717a597e92a684ff7b0adb5edf0ebb0649cfc23b
BrajGopalMaity <brajgopal90@gmail.com>
2017-04-11 03:47:53 -0400

    function added

--- Twitter_data.py
+++ Twitter_data.py
@@ -36,6 +36,7 @@ def split_line(line):
 def filter_line(line, whitelist):
     return ''.join([ ch for ch in line if ch in whitelist ])
 
+
 '''
  read list of words, create index to word,
   word to index dictionaries
@@ -52,3 +53,27 @@ def index_(tokenized_sentences, vocab_size):
     # word2index
     word2index = dict([(w,i) for i,w in enumerate(index2word)] )
     return index2word, word2index, freq_dist
+
+
+'''
+ filter too long and too short sequences
+    return tuple( filtered_ta, filtered_en )
+
+'''
+def filter_data(sequences):
+    filtered_q, filtered_a = [], []
+    raw_data_len = len(sequences)//2
+
+    for i in range(0, len(sequences), 2):
+        qlen, alen = len(sequences[i].split(' ')), len(sequences[i+1].split(' '))
+        if qlen >= limit['minq'] and qlen <= limit['maxq']:
+            if alen >= limit['mina'] and alen <= limit['maxa']:
+                filtered_q.append(sequences[i])
+                filtered_a.append(sequences[i+1])
+
+    # print the fraction of the original data, filtered
+    filt_data_len = len(filtered_q)
+    filtered = int((raw_data_len - filt_data_len)*100/raw_data_len)
+    print(str(filtered) + '% filtered from original data')
+
+    return filtered_q, filtered_a
