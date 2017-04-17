7b4329137e5126cd1a890f78d5bf4475c2d51c68
BrajGopalMaity <brajgopal90@gmail.com>
2017-04-17 01:49:42 -0400

    filter lines added

--- Final_data.py
+++ Final_data.py
@@ -102,3 +102,8 @@ def read_lines(filename):
 
 def split_line(line):
     return line.split('.')
+
+
+#Filter according to the white list
+def filter_line(line, whitelist):
+    return ''.join([ ch for ch in line if ch in whitelist ])    
