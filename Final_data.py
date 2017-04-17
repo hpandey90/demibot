bea92ee4d41615bf06b52466f2dc674a642329b2
ashish4feb <katiyar.ashish1989@gmail.com>
2017-04-17 01:41:16 -0400

    adding NLDS dataset processing

--- Final_data.py
+++ Final_data.py
@@ -28,3 +28,28 @@ import pickle
 
 def get_file(filename):
     return open(filename,"a+", encoding='utf-8', errors='ignore')
+
+
+def split_n_create(filename):
+    lines = open(filename,encoding='utf-8').read().split('\n')[:-1]
+    target = get_file(FILENAME)
+    flag = 0
+    counter = 0
+    s = ""
+    for line in lines:
+        if line=="CUT TO:" or counter > 10:
+            flag = 1
+            counter = 0
+        elif flag==1:
+            _line = line.split(' ')
+            if _line[0].isupper() and len(_line[0])!=1:
+                if len(s)!=0:
+                    tar = re.sub("[\(].*?[\)]", "", s)
+                    tar = tar.strip()
+                    target.write(tar)
+                    target.write("\n")
+                    s = ""
+            elif len(line)!=0:
+                s = ' '.join([s,line])
+            counter = 0
+        counter += 1
