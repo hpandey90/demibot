e5c00e9614287446270b999e960b7344473e0f3c
ashish4feb <katiyar.ashish1989@gmail.com>
2017-04-23 02:18:37 -0400

    Window config done

--- demibot.py
+++ demibot.py
@@ -6,10 +6,17 @@ from tkinter import messagebox
 
 #Create a window frame
 window = Tk()
+#Set Icon
+window.iconbitmap(default='logo.ico')
 #Set Size
 window.geometry("500x600")
 #Set Style
 window.style = Style()
+#('clam', 'alt', 'default', 'classic')
+window.style.theme_use("clam")
+#Give title to window
+window.wm_title("demiBot")
+window.option_add('*font', 'Helvetica 11')
 
 
 window.mainloop()
@@ -20,7 +27,7 @@ def Enter_pressed(event):
     quest = input_field.get()
     messages.config(state=NORMAL)
     messages.configure(font=UserFont)
-    
+
     messages.insert(INSERT, 'User : \n%s\n' % quest,'tag-left')
     messages.yview_moveto(messages.yview()[1])
     input_user.set('')
