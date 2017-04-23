d517126a4a5fd6ef21374dff575333b71f8ec726
ashish4feb <katiyar.ashish1989@gmail.com>
2017-04-23 02:00:23 -0400

    UI file with name demibot

---
+++ demibot.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter.font import Font
+from ttk import *
+from tkinter.scrolledtext import ScrolledText
+from tkinter import messagebox
+
+window = Tk()
+
+window.mainloop()
