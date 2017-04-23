cb2b97da1e84e01e279b1f4f8dd2595b9b1e8a85
himanshu pandey <hpandey90@gmail.com>
2017-04-23 02:54:14 -0400

    get model file modfied

---
+++ get_model.py
@@ -0,0 +1,11 @@
+from urllib.request import urlretrieve
+import zipfile
+import sys
+print("Downloading Data")
+def reporthook(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(totalsize)), readsofar, totalsize)
+        sys.stderr.write(s)
