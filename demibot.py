156dd7946b71fd16fa196b048a93d4498fe78d48
ashish4feb <katiyar.ashish1989@gmail.com>
2017-04-23 02:15:37 -0400

    Window created

--- demibot.py
+++ demibot.py
@@ -4,6 +4,12 @@ from ttk import *
 from tkinter.scrolledtext import ScrolledText
 from tkinter import messagebox
 
+#Create a window frame
 window = Tk()
+#Set Size
+window.geometry("500x600")
+#Set Style
+window.style = Style()
+
 
 window.mainloop()
