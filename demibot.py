87cb7dc7acd72c6b76878677c0aa2d36221589f6
himanshu pandey <hpandey90@gmail.com>
2017-04-23 02:14:55 -0400

    user messages added

--- demibot.py
+++ demibot.py
@@ -14,3 +14,8 @@ def Enter_pressed(event):
     quest = input_field.get()
     messages.config(state=NORMAL)
     messages.configure(font=UserFont)
+    
+    messages.insert(INSERT, 'User : \n%s\n' % quest,'tag-left')
+    messages.yview_moveto(messages.yview()[1])
+    input_user.set('')
+    print(quest)
