383585ac403994904bd3ecda335212be64f7d137
BrajGopalMaity <brajgopal90@gmail.com>
2017-04-23 02:23:35 -0400

    train

--- train.py
+++ train.py
@@ -5,3 +5,7 @@ import seq2seq
 # preprocessed data
 import Final_data
 import data_utils
+
+# load data from pickle and npy files
+metadata, idx_q, idx_a = Final_data.load_data(PATH='./Final_META/')
+(trainX, trainY), (testX, testY), (validX, validY) = data_utils.split_dataset(idx_q, idx_a)
