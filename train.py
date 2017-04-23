de976c79652066838e12b282ee770fb63f7167dc
BrajGopalMaity <brajgopal90@gmail.com>
2017-04-23 02:23:59 -0400

    train

--- train.py
+++ train.py
@@ -9,3 +9,11 @@ import data_utils
 # load data from pickle and npy files
 metadata, idx_q, idx_a = Final_data.load_data(PATH='./Final_META/')
 (trainX, trainY), (testX, testY), (validX, validY) = data_utils.split_dataset(idx_q, idx_a)
+
+# parameters
+xseq_len = trainX.shape[-1]
+yseq_len = trainY.shape[-1]
+batch_size = 32
+xvocab_size = len(metadata['idx2w'])
+yvocab_size = xvocab_size
+emb_dim = 512
