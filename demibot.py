e707e7eab1a2915ebbaeae0a1defa0ebe1ab8498
himanshu pandey <hpandey90@gmail.com>
2017-04-23 02:13:21 -0400

    press event added

--- demibot.py
+++ demibot.py
@@ -7,3 +7,10 @@ from tkinter import messagebox
 window = Tk()
 
 window.mainloop()
+
+
+
+def Enter_pressed(event):
+    quest = input_field.get()
+    messages.config(state=NORMAL)
+    messages.configure(font=UserFont)
